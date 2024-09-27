25aa01a8a6b1e623a22e88c61b853cf513f29f56
Marcos Oliveira <Mpe.oli@proton.me>
2024-09-27 15:23:41 -0300

    Create title, sale, film, series and timeCalculator

---
+++ src/Principal.java
@@ -0,0 +1,43 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+
+        meuFilme.setNome("The GodFather");
+        meuFilme.setAnoDeLancamento(1972);
+        meuFilme.setDuracaoEmMinutos(175);
+
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avaliaFilme(5.7);
+        meuFilme.avaliaFilme(10);
+        meuFilme.avaliaFilme(8);
+        System.out.println("Total de Avaliacões: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.printf("Media das Avaliações: %.2f", meuFilme.getMedia());
+
+        Serie sonsOfAnarchy = new Serie();
+        sonsOfAnarchy.setNome("Sons of Anarchy");
+        sonsOfAnarchy.setAnoDeLancamento(2008);
+        sonsOfAnarchy.exibeFichaTecnica();
+        sonsOfAnarchy.setTemporadas(7);
+        sonsOfAnarchy.setEpisodiosPorTemporadas(13);
+        sonsOfAnarchy.setMinutosPorEpisodio(50);
+        System.out.println("Duração total em minutos: " + sonsOfAnarchy.getDuracaoEmMinutos());
+
+        Filme outroFilme = new Filme();
+
+        outroFilme.setNome("Schindler's List");
+        outroFilme.setAnoDeLancamento(1993);
+        outroFilme.setDuracaoEmMinutos(195);
+
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(sonsOfAnarchy);
+        System.out.println(calculadora.getTempoTotal());
+    }
+}
---
+++ src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -0,0 +1,23 @@
+package br.com.alura.screenmatch.calculos;
+
+import br.com.alura.screenmatch.modelos.Titulo;
+
+public class CalculadoraDeTempo {
+    private int tempoTotal;
+
+    public int getTempoTotal() {
+        return tempoTotal;
+    }
+
+//    public void inclui(Filme filme) {
+//        this.tempoTotal += filme.getDuracaoEmMinutos();
+//    }
+//
+//    public void inclui(Serie serie) {
+//        this.tempoTotal += serie.getDuracaoEmMinutos();
+//    }
+
+    public void inclui(Titulo titulo) {
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
+    }
+}
