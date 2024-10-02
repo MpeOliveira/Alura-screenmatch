1d8afdbf92d7ff457ca899d1f752621f4fa35136
Marcos Oliveira <Mpe.oli@proton.me>
2024-10-02 00:24:30 -0300

    Create constructor matching super

--- src/br/com/alura/screenmatch/modelos/Serie.java
+++ src/br/com/alura/screenmatch/modelos/Serie.java
@@ -6,6 +6,10 @@ public class Serie extends Titulo {
     private int episodiosPorTemporadas;
     private int minutosPorEpisodio;
 
+    public Serie(String nome, int anoDeLancamento) {
+        super(nome, anoDeLancamento);
+    }
+
     public boolean isAtiva() {
         return ativa;
     }
