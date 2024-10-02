2007c3b4c70abbdf1361885c4ff262b9bace2888
Marcos Oliveira <Mpe.oli@proton.me>
2024-10-02 00:25:23 -0300

    Create constructor matching super toString() Overwrite

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -5,6 +5,10 @@ import br.com.alura.screenmatch.calculos.Rating;
 public class Filme extends Titulo implements Rating {
     private String diretor;
 
+    public Filme(String nome, int anoDeLancamento) {
+        super(nome, anoDeLancamento);
+    }
+
     public String getDiretor() {
         return diretor;
     }
@@ -16,5 +20,10 @@ public class Filme extends Titulo implements Rating {
     @Override
     public int getRating() {
         return (int) getMedia() / 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme: " + this.getNome() + " (" + this.getAnoDeLancamento() + ")";
     }
 }
